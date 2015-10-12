d702edc7be1bfdade0dcb447d8af3a1c2ddc54ec
Prelang Builder <builder@prelang.com>
2015-10-12 13:15:40 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Partial' to allow for create, update

--- app/admin/partial.rb
+++ app/admin/partial.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Partial do
+  permit_params :text, :background_color
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
